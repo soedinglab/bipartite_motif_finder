dbfefed38411b8caa38166157e01cafe136bfbc1
Salma Sohrabi <salma.sohrabi@yahoo.com>
2020-01-27 14:00:27 +0100

    new definition of zb

--- assign_zb_E_derivatives.c
+++ assign_zb_E_derivatives.c
@@ -3,28 +3,29 @@
 
 void assign_zb_E_derivatives_c(long* x, int i, int inx, double* za, double* zb, int L, int l, double l_p,
                                  double* za_Ea_derivatives, double* zb_Ea_derivatives, double* za_Eb_derivatives, double* zb_Eb_derivatives,
-                                 double* Ea, double* Eb, double cab, double D)
+                                 double* Ea, double* Eb, double cab, double sf, double D, double sig)
                                  {
     int identical = (inx == x[i]);
     
     double der_b = zb_Eb_derivatives[inx*L+i-1];
     double der_a = zb_Ea_derivatives[inx*L+i-1];
+
+    double energy = exp(-Eb[x[i]]);
     int j;
     
     if (i == l-1)  {
-        der_b += -cab*identical*exp(-Eb[x[i]]);
+        der_b += -cab*identical*energy;
         der_a += 0;
     }
         
     else  {
         for (j=0; j<i-l+1; ++j)
         {
-            der_b += cb_c(i-j-l, cab, D, l_p) * ((za_Eb_derivatives[inx*L+j]*exp(-Eb[x[i]]) - za[j]*exp(-Eb[x[i]])*identical));
-            der_b += cab * (zb_Eb_derivatives[inx*L+j]*exp(-Eb[x[i]]) - zb[j]*exp(-Eb[x[i]])*identical);
-            
-            der_a += cb_c(i-j-l, cab, D, l_p) * za_Ea_derivatives[inx*L+j]*exp(-Eb[x[i]]);
-            der_a += cab * zb_Ea_derivatives[inx*L+j]*exp(-Eb[x[i]]);
-        } 
+            der_b += cb_c(i-j-l, sf, D, sig) * ((za_Eb_derivatives[inx*L+j]*energy - za[j]*energy*identical));
+            der_a += cb_c(i-j-l, sf, D, sig) * za_Ea_derivatives[inx*L+j]*energy;  
+        }
+        der_b += cab * (zb_Eb_derivatives[inx*L+i-l]*energy - zb[i-l]*energy*identical); 
+        der_a += cab * zb_Ea_derivatives[inx*L+i-l]*energy;
     }
     
     zb_Ea_derivatives[inx*L+i] = der_a;
@@ -35,12 +36,48 @@ void assign_zb_E_derivatives_c(long* x, int i, int inx, double* za, double* zb, 
  }
 
 
-double cb_c(int d, double cab, double D, double l_p)
+double inline cb_c(int d, double sf, double D, double sig)
 {   
     if (d < 0)
         return 0;
-    double sig = 1 / (3*(d+1)*l_p);
-    double gaussian = exp(-pow(D,2) / (2 * pow(sig, 2.))) / pow(2*M_PI*pow(sig,2),3/2);
-    return cab + gaussian ;
+    
+    double diff = d + 1 - D; 
+    double gaussian = exp(- (diff*diff) / (2.0 *sig*sig)) / (sig*sqrt(2*M_PI));
+    return gaussian*sf + 1 ;
+}
+
+double cb_D_derivative_c(int d, double sf, double D, double sig)
+{    
+    if (d < 0)
+        return 0;
+
+    double diff = d + 1 - D; 
+    double der = sf*diff*exp(-diff*diff/(2*sig*sig))/(sig*sig*sig*sqrt(2*M_PI));
+    return der;
+    
+}
+
+double cb_sig_derivative_c(int d, double sf, double D, double sig)
+{    
+    if (d < 0)
+        return 0;
+
+    double diff = d + 1 - D; 
+    double sig2 = sig*sig;
+    double der = diff*diff/(sig2*sig2) - 1/sig2;
+    der *= (sf*exp(-diff*diff/(2*sig2)))/sqrt(2*M_PI);
+    return der;
+    
+}
+
+double cb_sf_derivative_c(int d, double sf, double D, double sig)
+{    
+    if (d < 0)
+        return 0;
+
+    double diff = d + 1 - D; 
+    double der = sf*exp(-diff*diff/(2*sig*sig))/(sig*sqrt(2*M_PI));
+    return der;
+    
 }
 
--- assign_zb_E_derivatives.h
+++ assign_zb_E_derivatives.h
@@ -1,7 +1,10 @@
 #include <math.h>
 
-void assign_zb_E_derivatives_c(long*, int , int , double* , double* , int , int , double,
+void assign_zb_E_derivatives_c(long* , int , int , double* , double* , int , int , double ,
                                  double* , double* , double* , double* ,
-                                 double* , double* , double , double );
+                                 double* , double* , double, double, double, double);
 
 double cb_c(int, double, double, double);
+double cb_D_derivative_c(int, double, double, double);
+double cb_sig_derivative_c(int, double, double, double);
+double cb_sf_derivative_c(int, double, double, double);
