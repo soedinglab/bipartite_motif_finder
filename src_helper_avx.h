151ea8be050281d5c91ec3268b0ebba1e2fd1e06
Salma Sohrabi <salma.sohrabi@yahoo.com>
2020-03-09 15:50:09 +0100

    flipped the derivative matrix for energies

---
+++ src_helper_avx.h
@@ -0,0 +1,19 @@
+#include <math.h>
+typedef double cfloat_t;
+
+void assign_za_E_derivatives_c(long* , int , int , double* , double* , int , int , int,
+                                 double* , double* , double* , double* ,
+                                 double* , double* , double);
+                                 
+void assign_zb_E_derivatives_c(long* , int , int , double* , double* , int , int , int,
+                                 double* , double* , double* , double* ,
+                                 double* , double* , double, double, double, double);
+
+
+
+double cb_c(int, double, double, double);
+double cb_D_derivative_c(int, double, double, double);
+double cb_sig_derivative_c(int, double, double, double);
+double cb_sf_derivative_c(int, double, double, double);
+
+
