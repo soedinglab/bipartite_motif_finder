9e71db76339e661eabaf30454feb1acb83f1be45
Salma Sohrabi <salma.sohrabi@yahoo.com>
2020-03-27 18:48:57 +0100

    generalize the runs for in-vitro datasets

---
+++ load_libs.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import itertools
+import pandas as pd
+from scipy.optimize import fmin_l_bfgs_b
+from scipy.optimize import check_grad
+from scipy.special import logsumexp
+from Bio import SeqIO
+import random
+import multiprocessing
+import multiprocessing as mp
+import ctypes
+from sklearn.metrics import roc_curve, roc_auc_score
+import argparse
