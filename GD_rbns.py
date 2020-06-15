3e52e5269d7b79846e61245dc825cd6472813ec4
Salma Sohrabi <salma.sohrabi@yahoo.com>
2020-06-15 20:13:52 +0200

    adapt to new pipeline + seed all rands

--- GD_rbns.py
+++ GD_rbns.py
@@ -5,9 +5,9 @@
 # ### Implements the dynamic programming and the gradient descent
 
 #load functions from the files
-exec(compile(open('load_libs.py', 'rb').read(), 'load_libs.py', 'exec'))
-exec(compile(open('LL_avx.py', 'rb').read(), 'LL_avx.py', 'exec'))
-exec(compile(open('ADAM_func.py', 'rb').read(), 'ADAM_func.py', 'exec'))
+exec(compile(open('src/load_libs.py', 'rb').read(), 'src/load_libs.py', 'exec'))
+exec(compile(open('src/LL_avx.py', 'rb').read(), 'src/LL_avx.py', 'exec'))
+exec(compile(open('src/ADAM_func.py', 'rb').read(), 'src/ADAM_func.py', 'exec'))
 
 #parse input argument(s)
 parser = argparse.ArgumentParser()
@@ -19,32 +19,43 @@ factor_number = args.factor_number
 no_tries = args.no_tries
 
 
+#collect best concentration information
+def index_containing_substring(the_list, substring):
+    for i, s in enumerate(the_list):
+        if substring in s:
+              return i
+    return -1
+
+data= pd.read_csv('RBNS_summary.txt', sep='\t')
+factors = data.loc[:,'RBP']
+concentrations = [int(conc.split(' ')[0]) for conc in data.loc[:,'MostRConc']]
+    
+    
 #define paths for RBNS dataset files
 rbp_path = '../rbns_scratch/data'
-factors = np.sort(os.listdir(rbp_path)) #list available factors
-
 all_files = [os.listdir(os.path.join(rbp_path, factor)) for factor in factors]
-
-pulldown_files = [np.sort(files)[2] for files in all_files]  #take 320 concentration when available otherwise another concentration
 input_files = [[f for f in files if '_input_' in f][0] for files in all_files]
-concentrations = [f.split('_')[1] for f in pulldown_files] #see which concentration we took
 
+files_flat = [item for sublist in all_files for item in sublist]
 
 if factor_number in range(len(factors)):
     factor = factors[factor_number]
     concentration = concentrations[factor_number]
-    print(f'processing {factor}')
+    print(f'processing {factor}_{concentration}')
     bg = os.path.join(rbp_path,factor,input_files[factor_number])
-    pos = os.path.join(rbp_path,factor,pulldown_files[factor_number])
+    pos = os.path.join(rbp_path,factor,files_flat[index_containing_substring(files_flat, f'{factor}_{concentration}')])
 else:
     print('factor number exceeds limit')
     exit()
 
     
+random.seed(42)
 background_set = parse_fastq(bg)
-background_set   = [seq.replace('N', random.sample(['A','T','C','G'],1)[0]) for seq in background_set]
+print(f'background set has {len(background_set)} sequences')
+background_set = [seq.replace('N', random.sample(['A','T','C','G'],1)[0]) for seq in background_set]
 
 positive_set = parse_fastq(pos)
+print(f'positive set has {len(positive_set)} sequences')
 positive_set = [seq.replace('N', random.sample(['A','T','C','G'],1)[0]) for seq in positive_set]
 
 bg_train, bg_valid = partition(background_set, 2)
@@ -53,6 +64,7 @@ pos_train, pos_valid = partition(positive_set, 2)
 
 # ### ADAM optimization
 for i in range(no_tries):
+    np.random.seed(i)
     Ea = np.random.normal(loc=12.0, scale=1.0, size=len(kmer_inx))
     Eb = np.random.normal(loc=12.0, scale=1.0, size=len(kmer_inx))
     sf = np.log(10000)
@@ -60,21 +72,17 @@ for i in range(no_tries):
     sig = np.log(np.random.uniform(1,15))
 
     parameters = np.concatenate([x.ravel() for x in [Ea, Eb, np.array([sf, D, sig])]])
-
-    identifier = random.randint(1,999)
-    n=4000
     
-    red_thr = 20
     var_thr = 0.03
     
     seq_per_batch = 500
     
-    plot_name = '%s_%s_%d_ADAM'%(factor, concentration, identifier)
+    file_name = f'{factor}_{concentration}_batchsize{seq_per_batch}_{i}'
 
     maxiter=1000
     x_opt = optimize_adam(pos_train, bg_train, 
-                          random.sample(pos_valid, n), random.sample(bg_valid, n), 
-                          red_thr, var_thr, sequences_per_batch=seq_per_batch, 
+                          pos_valid, bg_valid, 
+                          var_thr, sequences_per_batch=seq_per_batch, 
                           max_iterations=maxiter, evaluate_after=4000)
 
 
