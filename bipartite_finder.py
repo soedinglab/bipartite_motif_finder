f16f5f3954333ff1d565774c2c212196d9e29e13
Salma Sohrabi <salma.sohrabi@yahoo.com>
2020-12-24 09:06:50 +0100

    restrict number of sequences based on max_iter

--- bipartite_finder.py
+++ bipartite_finder.py
@@ -25,7 +25,7 @@ parser.add_argument('--batch_size', action="store", type=int, default=512, help=
 parser.add_argument('--max_iterations', action="store", type=int, default=1000, help='max number of iterations before stopping ADAM')
 parser.add_argument('--no_cores', action="store", type=int, default=4, help='the numbers of processors to be used in parallel')
 
-
+print("Hello")
 args = parser.parse_args()
 
 PSsequences_path = args.sequences
@@ -40,6 +40,8 @@ var_thr = args.var_thr
 batch_size = args.batch_size
 max_iterations = args.max_iterations
 no_cores = args.no_cores
+
+evaluate_after = 8
 
 #### REMINDER: TAKE CARE OF U VS T
 use_u = False
@@ -63,8 +65,16 @@ if not test:
     ps_sequences = parse_sequences(file_name=PSsequences_path, input_type=input_type, use_u=use_u)
     bg_sequences = parse_sequences(file_name=BGsequences_path, input_type=input_type, use_u=use_u)
 
+
     print('number of enriched sequences: ', len(ps_sequences))
     print('number of background sequences: ', len(bg_sequences))
+    
+    #only keep the number according to max_iterations when too many seqs
+    ps_sequences = ps_sequences[:min(batch_size*max_iterations*evaluate_after, len(ps_sequences))]
+    bg_sequences = bg_sequences[:min(batch_size*max_iterations*evaluate_after, len(bg_sequences))]
+
+    print('After shortlisting, number of enriched sequences: ', len(ps_sequences))
+    print('After shortlisting, number of background sequences: ', len(bg_sequences))
 
     # ### ADAM optimization
     for i in range(0, no_tries):
@@ -77,13 +87,19 @@ if not test:
         
         #initialize with a seed, makes results reproducible
         np.random.seed(i)
-        
+        print(f'ADAM optimization round {i}')
         #random initialization of parameters
         Ea = np.random.normal(loc=12.0, scale=1.0, size=4**core_length)
         Eb = np.random.normal(loc=12.0, scale=1.0, size=4**core_length)
         sf = np.log(10000) #scaling factor is log transformed in the algorithm
         r = np.log(np.random.uniform(1,5))
         p = np.log(np.random.uniform(0,1))
+
+
+        #### REMOVE
+        r = 1
+        p = 10
+        ###########
 
         parameters = np.concatenate([x.ravel() for x in [Ea, Eb, np.array([sf, r, p])]])
         
@@ -95,7 +111,7 @@ if not test:
                                 var_thr=var_thr,
                                 sequences_per_batch=seq_per_batch, 
                                 max_iterations=max_iterations, 
-                                evaluate_after=8,
+                                evaluate_after=evaluate_after,
                                 no_cores=no_cores,
                                 save_files=True,
                                 file_name=file_prefix
@@ -120,7 +136,7 @@ else:
     path_to_dir, param_file_names = os.path.split(parameters_path)
     
     # find all corresponding parameter files
-    param_files = [s for s in os.listdir(path_to_dir) if s.startswith('param_file_names')]
+    param_files = [s for s in os.listdir(path_to_dir) if s.startswith(f'{param_file_names}') & s.endswith('txt')]
 
     #read the parameters
     params = read_params([os.path.join(path_to_dir, param_file) for param_file in param_files])
@@ -131,6 +147,6 @@ else:
 
     #calculate auroc and ap scores
     kmer_inx = generate_kmer_inx(core_length)
-    y_scores = predict(seq_test, tetha, core_length, kmer_inx)
+    y_scores = predict(sequences, tetha, core_length, kmer_inx)
 
-    np.savetxt(fname=f'{output_prefix}.scores', X=y_scores)
+    np.savetxt(fname=f'{output_prefix}.predictions', X=y_scores)
